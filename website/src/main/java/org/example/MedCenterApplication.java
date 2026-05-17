4a28a72b4fbf6c6d40931b628407d0748df43545
na-bi-ma <migpol2014@gmail.com>
2026-05-18 02:22:45 +0300

    основа проекта
    Создана структура Spring Boot и настроены зависимости

---
+++ website/src/main/java/org/example/MedCenterApplication.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedCenterApplication.class, args);
+    }
+}
